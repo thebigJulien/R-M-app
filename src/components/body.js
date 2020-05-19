bf678932dc4292974619c1757cb11650f70abc3b
Julien Montagnon <jumontagnon@gmail.com>
2020-05-19 17:36:43 +0200

    Scarry T

--- src/components/body.js
+++ src/components/body.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import Rick from '../assets/rick.png';
+import Card from './card';
 
 const Body = () => {
     return(
-        <div className="body">
-            <div className="card">
-                <div className="card-img">
-                <img></img>
-                </div>
-
-                <div className="card-text">
-                    <p>name</p>
-                    <p>species</p>
-                    <p>gender</p>
-                    <p>location</p>
-                </div>
-            </div>
-        </div>
+        <Card></Card>
     )
 };
 
--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Characters from './characters';
 
 const Card = () => {
+
+    let data = Characters.results;
+console.log(data);
+    let eachChar = data.map((char) => 
+        <div className="card">
+            <img src= { char.image } className="card-img"></img>
+            <div className="card-text">
+                <h2>{ char.name }</h2>
+                <p>SPECIES: { char.species }</p>
+                <p>GENDER: { char.gender } </p>
+                <p>STATUS: { char.status }</p>
+                <p>ORIGIN: { char.origin.name }</p>
+                <p>LOCATION: { char.location.name }</p>
+            </div>
+        </div>
+            
+)
+
     return(
-        <div></div>
-    )
+        <div className="body">
+            { eachChar }
+        </div>    
+        )
 };
 
 export default Card
