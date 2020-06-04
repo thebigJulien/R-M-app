6358fed4fbd5075f1668417bfa9ad401e6cf4adf
Julien Montagnon <jumontagnon@gmail.com>
2020-06-04 10:10:09 +0200

    Updates

---
+++ precache-manifest.1e9a3d9bc184356aec47bf3d33f1a21a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1167b2907c05439f424578213ddf907b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "156fc9fb18e6758d1d15",
+    "url": "./static/css/main.534ef14a.chunk.css"
+  },
+  {
+    "revision": "168ea0ccc93891d7c56c",
+    "url": "./static/js/2.3d771e4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3d771e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156fc9fb18e6758d1d15",
+    "url": "./static/js/main.7ba228ad.chunk.js"
+  },
+  {
+    "revision": "b668f6bb3e597b6fd039",
+    "url": "./static/js/runtime-main.a70816e4.js"
+  },
+  {
+    "revision": "a3a06e95bc1e77bbfbc4abfb6a929fd8",
+    "url": "./static/media/scary-t.a3a06e95.png"
+  }
+]);
